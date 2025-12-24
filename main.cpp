e5416d505e9f490f5d23f81f7bff7caa9a693ace
JinggaJilca <jinggajilca@gmail.com>
2025-12-24 12:19:07 +0700

    Final

--- main.cpp
+++ main.cpp
@@ -17,33 +17,5 @@ int main(){
     dataPeminjamDummy(LP);
     showMenu(LB, LP, LR);
 
-    //=== TESTING DG DATA DUMMY ===
-    // dataBukuDummy(LB);
-    // dataPeminjamDummy(LP);
-
-    // showAllPeminjam(LP);
-    // showAllBuku(LB);
-
-    // dataRelasiDummy(LR, LB, LP);
-    // showAllRelasiBuku(LR, "1");
-    // showAllRelasiBuku(LR, "2");
-    // showAllRelasiBuku(LR, "3");
-    // showAllRelasiBuku(LR, "4");
-    // showAllRelasiBuku(LR, "5");
-
-    // inputNBuku(LB);
-    // inputNPeminjam(LP);
-
-    // string inputIdBuku, inputIdPeminjam;
-    // cout << "Input ID Buku: ";
-    // cin >> inputIdBuku;
-    // cout << "Input ID Peminjam: ";
-    // cin >> inputIdPeminjam;
-
-    // aksiPinjamBuku(LR,LP,LB, inputIdPeminjam, inputIdBuku);
-    // showAllRelasiBuku(LR, LB, inputIdBuku);
-
-
-
     return 0;
 }
